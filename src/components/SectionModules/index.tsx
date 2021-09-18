561cd73105f9bb03a8968fe293af1ce1a349d9ad
Alan Vidal <alan.fonseca@kumulus.com.br>
2021-09-18 16:45:57 -0300

    Integração Landing Page Section Projects à Section Agenda

--- src/components/SectionModules/index.tsx
+++ src/components/SectionModules/index.tsx
@@ -4,17 +4,21 @@ import Heading from 'components/Heading'
 import Container from 'components/Container'
 import CardModule from 'components/CardModule'
 
-import content from './content'
 import * as S from './styles'
 
-const SectionModules = () => (
+import { SectionModelsProps } from 'types/api'
+
+type Props = {
+  SectionModels: SectionModelsProps
+}
+const SectionModules = ({ SectionModels }: Props) => (
   <Container>
-    <Heading reverseColor>Módulos deste curso</Heading>
+    <Heading reverseColor>{SectionModels.title}</Heading>
 
     <S.Content>
-      {content.map(({ title, subtitle, description }, index) => (
-        <CardModule key={index} title={title} subTitle={subtitle}>
-          <div dangerouslySetInnerHTML={{ __html: description }} />
+      {SectionModels.models.map((iten, index) => (
+        <CardModule key={index} title={iten.title} subTitle={iten.subTitle}>
+          <div dangerouslySetInnerHTML={{ __html: iten.description }} />
         </CardModule>
       ))}
     </S.Content>
--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,11 +1,84 @@
 const GET_LANDING_PAGE = /*GraphQL*/ `
-query GET_LANDING_PAGE{
-  landingPage{
-    logo{
+fragment logo on LandingPage {
+  logo {
+    alternativeText
+    url
+  }
+}
+
+fragment header on LandingPage {
+  header {
+    title
+    description
+    button {
+      label
+      url
+    }
+    image {
       alternativeText
       url
     }
   }
 }
+
+fragment sectionAboutProject on LandingPage {
+  sectionAboutProject {
+    title
+    descrpition
+    image {
+      alternativeText
+      url
+    }
+  }
+}
+
+fragment sectionTech on LandingPage {
+  sectiontech {
+    title
+    techIcon {
+      title
+      icon {
+        url
+      }
+    }
+  }
+}
+fragment sectionConcepts on LandingPage {
+  sectionConcepts {
+    title
+    concepts {
+      title
+    }
+  }
+}
+
+fragment sectionModels on LandingPage {
+  SectionModels {
+    title
+    models {
+      title
+      subTitle
+      description
+    }
+  }
+}
+fragment sectionAgenda on LandingPage{
+sectionAgenda{
+  title
+  description
+}
+}
+query GET_LANDING_PAGE {
+  landingPage {
+    ...logo
+    ...header
+    ...sectionAboutProject
+    ...sectionTech
+    ...sectionConcepts
+    ...sectionModels
+    ...sectionAgenda
+  }
+}
 `
+
 export default GET_LANDING_PAGE
